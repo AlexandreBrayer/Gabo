ff05024ba8509d3ae0a0029a041df98ac836572e
Alexandre <alexandre@jsc-trade?com>
2022-09-19 17:26:21 +0200

    [ADD] pagination

--- src/pages/Profile.svelte
+++ src/pages/Profile.svelte
@@ -5,6 +5,8 @@
   import UserCard from "../lib/UserCard.svelte";
   import { navigate } from "svelte-routing";
   import Loader from "../lib/Loader.svelte";
+  import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+  import { FontAwesomeIcon } from "fontawesome-svelte";
   function logout() {
     $token = null;
     $userId = null;
@@ -14,9 +16,20 @@
   }
 
   let userInfos = null;
+  let page = 0;
   let games = [];
+
+  function nextPage() {
+    page++;
+    getGames();
+  }
+  function previousPage() {
+    page--;
+    getGames();
+  }
+
   function getGames() {
-    fetch(import.meta.env.VITE_API + "/game/mygames", {
+    fetch(import.meta.env.VITE_API + "/game/mygames?page=" + page, {
       headers: {
         Authorization: $token,
       },
@@ -56,7 +69,28 @@
   {#each games as game}
     <ProfileGameCard {game} />
   {/each}
+  <div class="navigator">
+    <button on:click={previousPage} disabled={page < 1} class="button is-info">
+      <FontAwesomeIcon icon={faAngleLeft} /></button
+    >
+    <button on:click={nextPage} class="button is-info">
+      <FontAwesomeIcon icon={faAngleRight} /></button
+    >
+  </div>
   <button class="button is-danger m-4" on:click={logout}>Logout</button>
 {:else}
   <Loader />
 {/if}
+
+<style>
+  .navigator {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 10px;
+    width: 90%;
+    max-width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
