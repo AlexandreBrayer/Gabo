090493ee4c767642ebeef14c9f2e49be87df0843
Alexandre <alexandre@jsc-trade?com>
2022-09-19 15:21:46 +0200

    [CHG] group store behavior

--- src/stores/gameStore.js
+++ src/stores/gameStore.js
@@ -1,5 +1,5 @@
 import { writable } from "svelte/store";
 
-export const groupStore = writable(null);
+export const groupStore = writable([]);
 export const roundsStore = writable([]);
 export const gameStore = writable(null);
